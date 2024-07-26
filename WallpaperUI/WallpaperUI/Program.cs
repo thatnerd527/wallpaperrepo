7e37ded87eb70d0aa99007aa58a9709c5709a50b
thatnerd527 <muhammadfadhlan4@gmail.com>
2024-07-26 20:22:35 +0700

    Co-authored-by: SlamTheDragon <SlamTheDragon@users.noreply.github.com>

--- WallpaperUI/WallpaperUI/Program.cs
+++ WallpaperUI/WallpaperUI/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.FluentUI.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components.Components.Tooltip;
 
 using WallpaperUI;
 
@@ -12,4 +13,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddFluentUIComponents();
 builder.Services.AddHttpClient();
+builder.Services.AddScoped<ITooltipService, TooltipService>();
 await builder.Build().RunAsync();
